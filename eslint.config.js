a7d4a14ea5c37b92061c68d4e5996a2ccba2a532
dengsinan <1754510323@qq.com>
2026-03-18 20:21:26 +0800

    chore: 配置 unplugin-auto-import 和 unplugin-icons

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,13 @@
+import { createRequire } from 'node:module'
 import antfu from '@antfu/eslint-config'
 
+const require = createRequire(import.meta.url)
+const { globals } = require('./.eslintrc-auto-import.json')
+
 export default antfu({
-  react: true, // 明确开启 React 支持
+  react: true,
+}, {
+  languageOptions: {
+    globals,
+  },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import babel from '@rolldown/plugin-babel'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import AutoImport from 'unplugin-auto-import/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -8,6 +12,39 @@ export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+      ],
+      imports: ['react', 'react-dom', {
+        from: 'react',
+        imports: ['FC'],
+        type: true,
+      }],
+      resolvers: [
+        IconsResolver({
+          prefix: 'icon',
+          extension: 'jsx',
+          customCollections: ['custom'],
+        }),
+      ],
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Icons({
+      compiler: 'jsx',
+      jsx: 'react',
+      scale: 1.0,
+      customCollections: {
+        custom: FileSystemIconLoader(
+          './src/assets/icons',
+          // eslint-disable-next-line e18e/prefer-static-regex
+          svg => svg.replace(/^<svg /, '<svg fill="currentColor" '),
+        ),
+      },
+
+    }),
   ],
   resolve: {
     alias: {
